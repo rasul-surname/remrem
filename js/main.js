d0008eb6bf04a655faa39b731eb110befc97aeeb
Расул <67557922+rasul-surname@users.noreply.github.com>
2021-05-31 14:39:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,75 @@
+// Добавление и удаление класса
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger, .header__menu').toggleClass('active');
+	});
+	$('.header__link').click(function (event) {
+		$('.header__link, .header__burger, .header__menu').removeClass('active');
+	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 150) {
+			$('.header').addClass('fixed');
+		}
+		else if ($(this).scrollTop() < 150) {
+			$('.header').removeClass('fixed');
+		}
+	});
+});
+
+// Плавное перемещение по ссылке
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function (event) {
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		});
+	});
+};
+
+new Swiper('.swiper-container', {
+	slidesPerView: 'auto',
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	breakpoints: {
+		1024: {
+			slidesPerView: 3,
+		},
+		1226: {
+			slidesPerView: 4,
+		}
+	}
+
+});
+
+new Swiper('.reviews-slider', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	}
+
+
+});
+
+var $header = $(".header")
+var scroll = 10
+var active = "active"
+$(window).scroll(function () {
+	if ($(window).scrollTop() > scroll) {
+		$header.addClass(active)
+	} else {
+		$header.removeClass(active)
+	}
+});
+
